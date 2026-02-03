659be2718b1fe673bb18a40592224bcfff7e4fba
Atreya Chandan <atreyachandan786@gmail.com>
2026-02-03 09:00:26 +0530

    App deployed

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: "http://192.168.1.43:5000", // Own Laptop IP, changes periodically
+  baseURL: "https://geo-attendance-tracker.onrender.com",
   headers: {
     "Content-Type": "application/json",
   },
