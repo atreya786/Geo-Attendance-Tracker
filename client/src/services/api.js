2dc13a901a13cdd5b941e156d75a3a63c1922821
Atreya Chandan <atreyachandan786@gmail.com>
2026-02-05 14:29:56 +0530

    2nd production build

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,8 +1,16 @@
 import axios from "axios";
 
+// 1. Define your environments
+const PROD_URL = "https://geo-attendance-tracker.onrender.com";
+const DEV_URL = "http://192.168.1.40:5000";
+
+// 2. Automatically pick the right one
+// If you are running 'npx expo start', this uses DEV_URL.
+// If you run 'eas build --profile preview', this uses PROD_URL.
+const baseURL = __DEV__ ? DEV_URL : PROD_URL;
+
 export const api = axios.create({
-  // baseURL: "https://geo-attendance-tracker.onrender.com",
-  baseURL: "http://192.168.1.40:5000",
+  baseURL: baseURL,
   headers: {
     "Content-Type": "application/json",
   },
